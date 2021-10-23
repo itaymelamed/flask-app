0f64576d22fea1f715b08e90439cdf2eb165fe7e
itaymelamed <itaymelamed@ymail.com>
2021-10-23 06:30:11 +0300

    added comose

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def test_app():
+def test_app1():
     assert requests.get("http://app1:5000").status_code == 200
 
 
-def test_app():
+def test_app2():
     assert requests.get("http://app2:5000").status_code == 200
